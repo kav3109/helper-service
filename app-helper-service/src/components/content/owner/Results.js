bf87c56ce64e0affcea937e1cc6edded204648fa
Artest <kav3109@gmail.com>
2020-10-14 15:44:46 +0300

    results(20%)

--- app-helper-service/src/components/content/owner/Results.js
+++ app-helper-service/src/components/content/owner/Results.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import {GetQuestions} from '../../../core/service';
+import React, {useState} from 'react';
+import {FormattedMessage} from 'react-intl';
+import { useForm } from 'react-hook-form';
 
 function CreateSurvey() {
 
-    const data = GetQuestions();
+    const { register, handleSubmit } = useForm();
+    const [error, setError] = useState(false);
+    const idSurvey = localStorage.getItem('surveyId');
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log('submited');
-        // SendAnswers();
-        // Test();
+    function onSubmit(data) {
+        console.log('done');
     }
 
     return (
         <div className="content card w-50">
             <div className="card-body">
-                <form className="questions" onSubmit={handleSubmit}>
-                    {data.map((data) =>
-                        <p key={data.id}>{data.val}</p>
-                    )}
-                    <input type="submit" />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FormattedMessage id="app.participant.surveyId" defaultMessage="Survey ID">
+                        {(message) => <input type="text"
+                                             name="surveyID"
+                                             className="form-control question mx-auto mb-1"
+                                             placeholder={message}
+                                             value={idSurvey? idSurvey: undefined}
+                                             ref={register({required: true})}
+                        />}
+                    </FormattedMessage>
+                    {error?<p className="error">
+                        <FormattedMessage
+                            id = "app.participant.error"
+                            defaultMessage="No such survey!"
+                        />
+                    </p>:null}
+                    <FormattedMessage id="app.participant.start" defaultMessage="Start">
+                        {(message) => <button type="submit"
+                                              className="btn btn-primary mt-1"
+                                              onClick={handleSubmit}>{message}</button>}
+                    </FormattedMessage>
                 </form>
 
             </div>
